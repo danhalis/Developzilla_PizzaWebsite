d60099da2efd7d70eb432468f1c17b52a537ec5d
Palmar <60619342+Picard4@users.noreply.github.com>
2021-11-16 22:10:20 -0500

    Prepared the setup for each product menu

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -113,6 +113,38 @@ namespace PizzaWebsite.Controllers
             return View();
         }
 
+        [HttpGet("Pizzas")]
+        public IActionResult Pizzas()
+        {
+            ViewBag.Title = "Pizza Menu";
+
+            return View();
+        }
+
+        [HttpGet("Drinks")]
+        public IActionResult Drinks()
+        {
+            ViewBag.Title = "Drink Menu";
+
+            return View();
+        }
+
+        [HttpGet("Burgers")]
+        public IActionResult Burgers()
+        {
+            ViewBag.Title = "Burger Menu";
+
+            return View();
+        }
+
+        [HttpGet("Sides")]
+        public IActionResult Sides()
+        {
+            ViewBag.Title = "Side Menu";
+
+            return View();
+        }
+
         [HttpGet("Privacy")]
         public IActionResult Privacy()
         {
