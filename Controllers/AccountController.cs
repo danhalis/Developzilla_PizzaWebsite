0ec0823d2cad11f32d24e345e9df31aa13c9d2aa
WonderScarf <tyler.messinak@gmail.com>
2021-11-27 19:01:23 -0500

    Added employee login functionality.
    - Login screen redone with a new box for selecting if you are an employee or not.
    - When box is checked and have an employees' role you are given a different outcome depending on the type of employee.
    -Still needs to redirect to a more fitting location.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PizzaWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PizzaWebsite.Controllers
+{
+    public class AccountController: Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                              SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> SignIn(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, viewModel.RememberMe, false);
+                
+                if (result.Succeeded)
+                {
+
+                    if (viewModel.IsEmployee)
+                    {
+                        var user = await _userManager.FindByEmailAsync(viewModel.Email);
+                        var roles = await _userManager.GetRolesAsync(user);
+
+                        if (roles.Contains("Owner")) 
+                        {
+                            // Redirect to the homepage of the Admin Controller
+                            return RedirectToAction("Menu", "Home");
+                        }
+                        else if (roles.Contains("Manager"))
+                        {
+                            // Redirect to the homepage of the Manager Controller
+                            return RedirectToAction("Menu", "Home");
+                        }
+                        else if (roles.Contains("Cook"))
+                        {
+                            // Redirect to the homepage of the Cook Controller
+                            return RedirectToAction("Menu", "Home");
+                        }
+                        else if (roles.Contains("Deliverer"))
+                        {
+                            // Redirect to the homepage of the Deliverer Controller
+                            return RedirectToAction("Menu", "Home");
+                        }
+                        else if (roles.Contains("Front"))
+                        {
+                            // Redirect to the homepage of the Front Controller
+                            return RedirectToAction("Menu", "Home");
+                        }
+                        else
+                        {
+                            await _signInManager.SignOutAsync();
+                            ModelState.AddModelError(string.Empty, "The user is not an employee please uncheck the employee box to sign into the customer login.");
+                            return View(viewModel);
+                        }
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                ModelState.AddModelError(string.Empty, "Login Failed. Please make sure your account data is valid.");
+
+            }
+            return View(viewModel); 
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaWebsite.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [RegularExpression(@".*@.*\.\w{2,}", ErrorMessage = "Please enter a valid email address.")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+
+        [Display(Name = "Employee:")]
+        public bool IsEmployee { get; set; }
+
+        [Display(Name = "Remember Me:")]
+        public bool RememberMe { get; set; }
+    }
+}
