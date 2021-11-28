beaa076b408e23cb1fe4a8869a5f8286d1efa632
WonderScarf <tyler.messinak@gmail.com>
2021-11-28 16:28:28 -0500

    Refactored employee login, created views, controller and layout for employees.
    - Removed AccountController to better mesh with others' code.
    - Added an employee layout for design specific to employees
    - Added Employee views for each roles affected by permissions/roles given.
    - Implemented navigation bewtween employee views.
    - Removed LoginViewmodel and other redundant code.
    - Fixed some styling.
    - Commented when possible.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaWebsite.Controllers
+{
+    [Authorize(Roles = "Owner, Cook, Manager, Front, Deliverer")]
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Owner")]
+        public IActionResult Owner()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Owner, Manager")]
+        public IActionResult Manager()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles = "Front, Owner, Manager")]
+        public IActionResult Front()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Cook, Owner, Manager")]
+        public IActionResult Cook()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Deliverer, Owner, Manager")]
+        public IActionResult Deliverer()
+        {
+            return View();
+        }
+    }
+}
